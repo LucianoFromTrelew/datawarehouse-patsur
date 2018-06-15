dba17d93f9804d807b58ea1074875c6280fc6694
Luciano <lucianoserruya@gmail.com>
2018-06-15 15:45:58 -0300

    Agregar script para medio_pago (ACTUAL)

---
+++ src/cargas/sistema_actual/sql/medio_pago.sql
@@ -0,0 +1,7 @@
+INSERT INTO medio_pago VALUES (1, 'Efectivo', 0, 0, 0);
+INSERT INTO medio_pago VALUES (2, 'Crédito', 0, 0, 0);
+INSERT INTO medio_pago VALUES (3, 'Débito', 0, 0, 0);
+INSERT INTO medio_pago VALUES (4, 'Cheque', 0, 0, 0);
+INSERT INTO medio_pago VALUES (5, 'Depósito', 0, 0, 0);
+INSERT INTO medio_pago VALUES (6, 'MercadoPago', 0, 0, 0);
+INSERT INTO medio_pago VALUES (7, 'Bitcoin', 0, 0, 0);
