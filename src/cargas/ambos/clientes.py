40f3f79f0739cace72c07aa2e43b72d778dc2243
Pablo Toledo <pabloatm980@gmail.com>
2018-06-18 21:12:57 -0300

    Add script and sql files for shared clients

---
+++ src/cargas/ambos/clientes.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from random import randint
+
+
+SQL_FILE_VIEJO = './sql/clientes_viejo.sql'
+SQL_FILE_NUEVO = './sql/clientes_nuevo.sql'
+
+
+def main():
+    clientes = pd.read_csv('./clientes.csv', sep=';')
+
+    id_cliente_nuevo = 1
+    id_cliente_viejo = 10000
+
+    clientes_nuevos = ''
+    clientes_viejos = ''
+
+    for index, row in clientes.iterrows():
+        tipo_cliente = randint(0, 4)
+
+        clientes_viejos += 'INSERT INTO "cliente" (nro_cliente,nombre,tipo,direccion) VALUES ({},\'{}\',{},\'{}\');\n'.format(
+                id_cliente_viejo,
+                row['nombre'],
+                tipo_cliente,
+                row['direccion']
+            )
+
+        clientes_nuevos += 'INSERT INTO "cliente" (cod_cliente,nombre,cod_tipo,direccion) VALUES ({},\'{}\',{},\'{}\');\n'.format(
+                id_cliente_nuevo,
+                row['nombre'],
+                tipo_cliente,
+                row['direccion']
+            )
+
+        id_cliente_viejo += 1
+        id_cliente_nuevo += 1
+
+    with open(SQL_FILE_NUEVO, 'w') as f:
+        f.write(clientes_nuevos)
+        f.close()
+
+    with open(SQL_FILE_VIEJO, 'w') as f:
+        f.write(clientes_viejos)
+        f.close()
+
+    print(clientes_viejos)
+    print(clientes_nuevos)
+
+
+if __name__ == "__main__":
+    main()
