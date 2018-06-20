a23f2cc99e6f2723c8499f9a39eaf465237ccafc
Luciano <lucianoserruya@gmail.com>
2018-06-20 00:02:05 -0300

    Agregar regiones

---
+++ src/cargas/datawarehouse/region.sql
@@ -0,0 +1,5 @@
+INSERT INTO region VALUES (1, 'Patagonia');
+INSERT INTO region VALUES (2, 'Pampa');
+INSERT INTO region VALUES (3, 'Cuyo');
+INSERT INTO region VALUES (4, 'Noreste');
+INSERT INTO region VALUES (5, 'Noroeste');
