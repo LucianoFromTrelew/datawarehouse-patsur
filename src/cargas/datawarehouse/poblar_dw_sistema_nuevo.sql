ed8bde7648aa65a1722c9e0f4f67eff96012ac19
Luciano <lucianoserruya@gmail.com>
2018-06-24 20:21:31 -0300

    Agregar conexión y desconexión al sistema actual
    La conexión con el sistema actual en la función para poblar el DW se
    hace dentro de la función, lo mismo para la desconexión al final de la
    función

--- src/cargas/datawarehouse/poblar_dw_sistema_nuevo.sql
+++ src/cargas/datawarehouse/poblar_dw_sistema_nuevo.sql
@@ -1,11 +1,13 @@
-SELECT DBLINK_CONNECT(
-	'sistema_actual',
-	'host=172.40.0.20 port=5432 password=admin user=admin dbname=facturacion'
-);
 
 create or replace function poblar_datawarehouse_sistema_nuevo(anio integer, mes integer) returns integer as
 $$
 begin
+
+    PERFORM DBLINK_CONNECT(
+        'sistema_actual',
+        'host=172.40.0.20 port=5432 password=admin user=admin dbname=facturacion'
+    );
+
 	-- crear tabla temporaria
 	create table tmp_ventas as
 	select
@@ -85,6 +87,7 @@ begin
 	and id_factura not in (select id_factura from venta);
 	
 	drop table tmp_ventas;
+    PERFORM DBLINK_DISCONNECT('sistema_actual');
 	return 0;
 end;
 $$ language plpgsql;
