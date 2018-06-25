563f79870d210794d5dc6d0ad688dca3195bb8c8
Luciano <lucianoserruya@gmail.com>
2018-06-25 16:05:20 -0300

    Remover archivos de otra rama
    El directorio 'consultas/' era de la rama 'agregar-consultas'

--- src/consultas/clientes_mas_compradores.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    cli.id_cliente AS "ID Cliente", 
-    cli.nombre AS "Nombre", 
-    CAST(SUM(ven.monto_vendido) AS bigint) AS "Total vendido", 
-    RANK() OVER (ORDER BY SUM(ven.monto_vendido) DESC) AS "Mayores compradores" 
-FROM venta AS ven 
-    JOIN cliente AS cli ON ven.id_cliente=cli.id_cliente 
-GROUP BY cli.id_cliente;
